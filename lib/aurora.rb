01c6a0d701b651990069a99aac381360f1a7af1e
Sam Richard <snugug@gmail.com>
2012-08-07 21:20:50 -0400

    RB file

---
+++ lib/aurora.rb
@@ -0,0 +1,11 @@
+require 'compass'
+require 'toolkit'
+require 'sassy-buttons'
+
+extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+Compass::Frameworks.register('aurora', :path => extension_path)
+
+module Aurora
+  VERSION = "0.2"
+  DATE = "2012-08-1"
+end
