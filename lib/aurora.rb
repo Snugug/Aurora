33ae14e185224126539d4e433045314bd82f321a
Sam Richard <snugug@gmail.com>
2012-09-12 11:20:07 -0400

    Install errors now fixed

--- lib/aurora.rb
+++ lib/aurora.rb
@@ -6,6 +6,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('aurora', :path => extension_path)
 
 module Aurora
-  VERSION = "0.7"
-  DATE = "2012-09-11"
+  VERSION = "0.7.1"
+  DATE = "2012-09-12"
 end
