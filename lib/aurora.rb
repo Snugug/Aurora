ca349a0c9b7425f6ee63966e76f387ac4695e111
Sam Richard <snugug@gmail.com>
2012-11-09 11:00:42 -0500

    Updated version number

--- lib/aurora.rb
+++ lib/aurora.rb
@@ -1,11 +1,12 @@
 require 'compass'
 require 'toolkit'
 require 'sassy-buttons'
+require 'compass-normalize'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('aurora', :path => extension_path)
 
 module Aurora
-  VERSION = "0.9.4"
-  DATE = "2012-10-10"
+  VERSION = "1.0"
+  DATE = "2012-11-09"
 end
