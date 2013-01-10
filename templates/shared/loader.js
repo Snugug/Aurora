1ccf2ef47374e4bec801a159cd3d3f0242777b77
Ian Carrico <Ian@FourKitchens.com>
2013-01-09 20:24:25 -0800

    Removing extra files

--- templates/shared/loader.js
+++
@@ -1,7 +0,0 @@
-// For more information on how to use Modernizr.load, see the official docs
-// http://modernizr.com/docs/#load
-
-Modernizr.load({
-  test: Modernizr.touch,
-  yep: 'hammer.js'
-});
