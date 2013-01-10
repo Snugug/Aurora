f1ed10acd35da486c2f5ec0f3b373ddccffbccb6
Sam Richard <snugug@gmail.com>
2013-01-09 21:50:36 -0500

    Swapped yepnope to Modernizr.load

---
+++ templates/shared/loader.js
@@ -0,0 +1,7 @@
+// For more information on how to use Modernizr.load, see the official docs
+// http://modernizr.com/docs/#load
+
+Modernizr.load({
+  test: Modernizr.touch,
+  yep: 'hammer.js'
+});
