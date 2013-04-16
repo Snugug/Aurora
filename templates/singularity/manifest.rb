dab8db2e5bdc6d7b4aff44a6fa527a1f76722c6c
Sam Richard <snugug@gmail.com>
2013-04-16 05:42:01 -0400

    Updated Singularity base and manifest

--- templates/singularity/manifest.rb
+++ templates/singularity/manifest.rb
@@ -1,4 +1,4 @@
-description "Aurora Base Theme"
+description "Aurora Sub Theme"
 no_configuration_file!
 
 # Add in custom config file.
@@ -7,7 +7,7 @@ file '../shared/config.rb.erb', :to => "config.rb", :erb => true
 # Add in README
 file '../shared/README-Sass.md',  :like => :stylesheet, :to => 'README.md'
 file '../shared/README-Partials.md',  :like => :stylesheet, :to => 'partials/README.md'
-file '../shared/README-templates.md', :to => "templates/README.md", :erb => true
+file '../shared/README-templates.md', :to => "templates/README.md"
 
 # ERB ALL the Drupal files!
 file '../shared/aurora.info.erb', :to => "#{options[:project_name] || File.basename(Compass.configuration.project_path)}.info", :erb => true
@@ -24,22 +24,21 @@ file '../shared/_mixins.scss', :like => :stylesheet, :to => 'partials/global/_mi
 file '../shared/_extendables.scss', :like => :stylesheet, :to => 'partials/global/_extendables.scss'
 
 file '../shared/_style-guide.scss', :like => :stylesheet, :to => 'partials/styleguide/_style-guide.scss'
-file '../shared/_print-style-guide.scss', :like => :stylesheet, :to => 'partials/styleguide/_print-style-guide.scss'
 
 file '../shared/_layout.scss', :like => :stylesheet, :to => 'partials/layout/_layout.scss'
-file '../shared/_print-layout.scss', :like => :stylesheet, :to => 'partials/layout/_print-layout.scss'
 
 file '../shared/_design.scss', :like => :stylesheet, :to => 'partials/design/_design.scss'
-file '../shared/_print-design.scss', :like => :stylesheet, :to => 'partials/design/_print-design.scss'
 
-file '../shared/_print.scss',  :like => :stylesheet, :to => '_print.scss'
+file '../shared/print.scss',  :like => :stylesheet, :to => 'print.scss'
 
-# JavaScript
-file '../shared/hammer.js',           :like => :javascript, :to => 'hammer.js'
+# Bower Awesomesauce
+file '../shared/bowerrc', :to => '.bowerrc'
+file '../shared/component.json.erb', :to => 'component.json', :erb => true
 
-# Behaviors
-file 'behaviors/box-sizing/boxsizing.htc'
-file 'behaviors/box-sizing/boxsizing.php'
+# Developer Consistency
+file '../shared/Gemfile', :to => 'Gemfile'
+file '../shared/editorconfig', :to => '.editorconfig'
+file '../shared/gitignore.txt', :to => ".gitignore"
 
 help %Q{
   Please contact Sam Richard with questions:
@@ -48,7 +47,9 @@ help %Q{
 }
 
 welcome_message %Q{
+  Singularity based Aurora Subtheme.
 
-  Aurora Base Theme using Singularity.
+  Please run `bundle install` from inside your new subtheme to isntall the required dependenecies. This will create a Gemfile.lock file, add this to your version control.
 
+  When you are ready start compiling, run `bundle exec compass watch` instead of simply `compass watch` to ensure you are compiling using your dependent Compass extensions.
 }
