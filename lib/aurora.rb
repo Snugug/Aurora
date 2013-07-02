8b6b4b661a4f592a1f48b4b91d4441dd6db4e760
Sam Richard <snugug@gmail.com>
2013-07-02 09:58:14 -0400

    Updated version number

--- lib/aurora.rb
+++ lib/aurora.rb
@@ -7,6 +7,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('aurora', :path => extension_path)
 
 module Aurora
-  VERSION = "3.0.7"
-  DATE = "2013-06-21"
+  VERSION = "3.0.8"
+  DATE = "2013-07-02"
 end
